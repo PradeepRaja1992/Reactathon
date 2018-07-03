2a1320f55c311a5573b08a0fd64e1f8ff5524b73
hskramesh <hskramesh@gmail.com>
2018-07-03 18:41:36 +0000

    added axios

--- my-app/src/Api/hackDetailsApi.js
+++ my-app/src/Api/hackDetailsApi.js
@@ -1,13 +1,27 @@
+import axios from 'axios';
+
 const fetchuserDetails = (dispatch) => {
     //api call
     dispatch({type :"DETAILS_FETCH_CALL"});
-    setTimeout(function(){
+    axios.get('https://randomuser.me/api/?results=50')
+    .then(response => {
         let data = [{"name":"Reactathon","description":"My description","duration":"3","registration_close_date": "8/3/2018","venue":"Chennai",
         "portfolio":"CMB","start_date":"2018-07-02","end_date":"2018-07-04","organiser_name":"Pradeep","organiser_contact":"r.pradeep@verizon.com"},
-        {"name":"Testathon","description":"My description","duration":"4","registration_close_date": "8/3/2018","venue":"Hyderbad","portfolio":"CMB",
+{"name":"Testathon","description":"My description","duration":"4","registration_close_date": "8/3/2018","venue":"Hyderbad","portfolio":"CMB",
         "portfolio":"CMB","start_date":"2018-07-02","end_date":"2018-07-04","organiser_name":"Pradeep","organiser_contact":"r.pradeep@verizon.com"}];
         dispatch({type :"DETAILS_FETCHED", payload : data});
-    },1000 );  
+        console.log(response);
+    })
+    .catch(error => {
+      console.log(error);
+    })
+    /*setTimeout(function(){
+       let data = [{"name":"Reactathon","description":"My description","duration":"3","registration_close_date": "8/3/2018","venue":"Chennai",
+        "portfolio":"CMB","start_date":"2018-07-02","end_date":"2018-07-04","organiser_name":"Pradeep","organiser_contact":"r.pradeep@verizon.com"},
+{"name":"Testathon","description":"My description","duration":"4","registration_close_date": "8/3/2018","venue":"Hyderbad","portfolio":"CMB",
+        "portfolio":"CMB","start_date":"2018-07-02","end_date":"2018-07-04","organiser_name":"Pradeep","organiser_contact":"r.pradeep@verizon.com"}];
+        dispatch({type :"DETAILS_FETCHED", payload : data});
+    },1000 );  */
 }
 
 export default {fetchuserDetails};
